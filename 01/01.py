5a70e614c70caa1bba26a620c251f4985f2cde20
Mike Ulch <mike@norcode.com>
2021-12-01 11:09:35 -0500

    day 1

--- 01/01.py
+++ 01/01.py
@@ -1,13 +1,26 @@
 from timeit import timeit
 
 lines = open('01.txt').readlines()
-# numbers = [int(x) for x in open('01.txt').readlines()]
+numbers = [int(x) for x in open('01.txt').readlines()]
+
+def get_increment_count(input):
+    increased = 0
+    for k,v in enumerate(input[1:], start=1):
+        if v > input[k-1]:
+            increased += 1
+    return increased
 
 def part1():
-    pass
+    print("increased {} times".format(get_increment_count(numbers)))
 
 def part2():
-    pass
+    newlist = []
+    for k,v in enumerate(numbers):
+        try:
+            newlist.append(v+numbers[k+1]+numbers[k+2])
+        except IndexError:
+            break
+    print("Increased {} times".format(get_increment_count(newlist)))
 
 if __name__ == '__main__':
     print("Part 1: ", end="")
