257f994ef70c624f5a9f76ffb0fe80cc597bace3
Mike Ulch <mike@norcode.com>
2021-12-10 13:25:41 -0500

    day 10

--- 10/10.py
+++ 10/10.py
@@ -1,10 +1,47 @@
 from timeit import timeit
 
-lines = open('01.txt').readlines()
-# numbers = [int(x) for x in open('01.txt').readlines()]
+lines = [x.strip() for x in open('10.txt').readlines()]
+
+tags = ['{}', '<>', '[]', '()']
+openers = ['{', '<', '[', '(']
+closers = [('}',1197), ('>', 25137), (']', 57), (')', 3)]
+
+
+def strip_matches(line):
+    found = True
+    while found:
+        found = False
+        for tag in tags:
+            if line.find(tag) != -1:
+                line = line.replace(tag, '')
+                found = True
+    return line
+
+def is_corrupt(line):
+    found = False
+    for c in line:
+        for k,v in closers:
+            if c == k:
+                found = True
+    return found
+
+def calculate(line):
+    for c in line:
+        for k,v in closers:
+            if c == k:
+                return v
+
 
 def part1():
-    pass
+    print(len(lines))
+    part1_lines = list(lines)
+    score = 0
+    for l in part1_lines:
+        stripped_line = strip_matches(l)
+        if is_corrupt(stripped_line):
+            score += calculate(stripped_line)
+    print(score)
+
 
 def part2():
     pass
